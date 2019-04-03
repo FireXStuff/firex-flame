7c311e388cb93e513bfae00bd4d91754b323dd34
Marc S Delahousse <mdelahou@cisco.com>
2019-04-03 11:29:10 -0400

    Initial version of setup.py to package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='firexk-flame',
+      version="0.1",
+      description='Core firex libraries',
+      url='https://github.com/FireXStuff/firex-flame',
+      author='Core FireX Team',
+      author_email='firex-dev@gmail.com',
+      license='BSD-3-Clause',
+      packages=['firexk_flame', ],
+      zip_safe=True,
+      install_requires=["flask"
+      ],)
