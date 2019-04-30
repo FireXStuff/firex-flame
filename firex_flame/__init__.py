6060fa22650c0ec33ab7d203e2971ccd9b8a4221
Marc S Delahousse <mdelahou@cisco.com>
2019-04-30 16:04:40 -0400

    Hello world for flame

---
+++ firex_flame/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+from firexapp.fileregistry import FileRegistry
+from firexapp.submit.uid import Uid
+
+FLAME_LOG_REGISTRY_KEY = 'FLAME_OUTPUT_LOG_REGISTRY_KEY'
+FileRegistry().register_file(FLAME_LOG_REGISTRY_KEY, os.path.join(Uid.debug_dirname, 'flame.stdout'))
--- firex_flame/launcher.py
+++ firex_flame/launcher.py
@@ -1,5 +1,7 @@
-
+import subprocess
 from celery.utils.log import get_task_logger
+from firex_flame import FLAME_LOG_REGISTRY_KEY
+from firexapp.fileregistry import FileRegistry
 from firexapp.submit.tracking_service import TrackingService
 
 logger = get_task_logger(__name__)
@@ -13,10 +15,18 @@ class FlameLauncher(TrackingService):
         # todo: add the ArgParse arguments
         pass
 
-    def start(self, args)->{}:
-        logger.debug("Start flame")
-        # todo: start the flame service and return the port
+    def start(self, args, uid=None, **kwargs)->{}:
+        # store sync state for later
         self.sync = args.sync
+
+        # assemble startup cmd
+        cmd = "firex_flame"
+
+        # start the flame service and return the port
+        logger.debug("Starting Flame...")
+        flame_stdout = FileRegistry().get_file(FLAME_LOG_REGISTRY_KEY, uid.logs_dir)
+        with open(flame_stdout, 'wb') as out:
+            subprocess.check_call(cmd, shell=True, stdout=out, stderr=subprocess.STDOUT)
 
     def __del__(self):
         if not self.sync:
