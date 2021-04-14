f1e25ee81d036f6332887b3e9abb12b307497083
Mohamed Abouelsaoud <mabouels@cisco.com>
2021-04-14 16:03:27 -0400

    Did the broken dependency got fixed, and hence no need for pinning?

--- setup.py
+++ setup.py
@@ -28,11 +28,7 @@ setup(name='firex_flame',
             "beautifulsoup4",
             "paramiko",
             "gevent-websocket",
-            "gevent",
-            # Without pinning, importlib 3.0.0 causes:
-            #     IsADirectoryError: [Errno 21] Is a directory: '/<path>/site-packages/firex_flame_ui'
-            "importlib-metadata==1.6.0",
-            "importlib-resources==1.0.2",
+            "gevent"
       ],
       package_data={
         'firex_flame': ['templates/*.html'],
