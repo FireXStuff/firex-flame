d0d127a5cd34e5204751d7733900b19ce694badf
Damir Jungic <djungic@cisco.com>
2023-02-08 06:17:45 -0800

    Revert "Pin beautifulsoup4 to avoid inaccurate warnings no longer being ignored."
    This reverts commit bbf4ec0b909f1c42509478775ee1864ef4338ada.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='firex_flame',
             # e.g 0.20.x means this server adheres to contract version 0.20 between UI and server.
             "firex_flame_ui<0.30",
             "requests",
-            "beautifulsoup4==4.11.1",
+            "beautifulsoup4",
             "paramiko",
             "gevent-websocket",
             "gevent",
