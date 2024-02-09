a2b7834dbc379c2249013dc981c666bdf086a4cc
Damir Jungic <djungic@cisco.com>
2024-02-09 03:49:51 -0800

    Save memory by deleting big fields from completed tasks and reloading these fields from task JSON files when required. Cache task queries and allow querying by query name instead of full query data.
    This reverts commit 4962b9e302a3808f1c30c12749c53377aa0894cf.

--- tests/unit_tests/event_file_processor_tests.py
+++ tests/unit_tests/event_file_processor_tests.py
@@ -1,24 +1,26 @@
 import os
 import unittest
 
-from firex_flame.event_aggregator import FlameEventAggregator
-from firex_flame.event_file_processor import process_recording_file, get_model_or_rec_full_task
+from firex_flame.controller import FlameAppController
+from firex_flame.event_file_processor import process_recording_file, get_model_or_rec_full_tasks_by_uuids
 
 parent = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 test_data_dir = os.path.join(parent, "data")
 
+def get_model_or_rec_full_task(logs_dir, uuid):
+    task_by_uuid = get_model_or_rec_full_tasks_by_uuids(logs_dir, [uuid])
+    return task_by_uuid[uuid]
 
 class EventFileProcessorTests(unittest.TestCase):
 
     def test_process_rec_file(self):
-        event_aggregator = FlameEventAggregator()
 
         recording_file = os.path.join(test_data_dir, 'nop', 'firex_internal', 'flame', 'flame.rec')
 
-        run_metadata = {}
-        process_recording_file(event_aggregator, recording_file, run_metadata)
+        controller = FlameAppController({})
+        process_recording_file(controller, recording_file)
 
-        self.assertEqual(2, len(event_aggregator.tasks_by_uuid), "Expected two tasks, root and nop.")
+        self.assertEqual(2, len(controller.graph.get_full_tasks_by_uuid()), "Expected two tasks, root and nop.")
         # TODO: assert on metadata once args are emitted in celery events by firexapp.
 
     def test_rec_full_task(self):
